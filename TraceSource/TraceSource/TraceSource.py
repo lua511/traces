e9b6a6e857c306f63187e38434c45357683f9f04
lwork.t450s <lwork.t450s@thinkpad>
2016-08-17 22:12:28 +0800

    tk startup

---
+++ TraceSource/TraceSource/TraceSource.py
@@ -0,0 +1,17 @@
+import Tkinter
+from Tkinter import *
+
+class CodeFile:
+    def __init__(self,filename):
+        self.fname = filename
+        self.ftime = []
+
+class   TimeTag:
+    def __init(self,timet,fname):
+        self.timet = timet
+        self.fname = fname
+
+root = Tkinter.Tk()
+can = Tkinter.Canvas(root,bg='white',height=600,width=800)
+can.pack()
+root.mainloop()
